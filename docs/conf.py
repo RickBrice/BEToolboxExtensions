459f1605ba9535d3ca1845af935e8c8c73005dfe
Rick Brice <bricer@wsdot.wa.gov>
2020-12-02 11:58:10 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import sphinx_rtd_theme
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
